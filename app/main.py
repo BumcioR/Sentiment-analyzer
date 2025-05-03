c63b6e072f5eca342f785467d1a613eb0db26e01
Bumi <ebumi@agh.edu.pl>
2025-05-03 19:51:36 +0200

    Add inference_class, update main

--- app/main.py
+++ app/main.py
@@ -2,6 +2,14 @@
 
 from fastapi import FastAPI
 from app.schemas import InferenceRequest, InferenceResponse
+import cloudpickle
+
+# Załaduj model przy starcie serwera (jeden raz, oszczędnie)
+with open("app/artifacts/inference_class.pkl", "rb") as f:
+    Inference = cloudpickle.load(f)
+
+# Inicjalizacja klasy predykcyjnej – uzupełnimy parametry, gdy je poznamy
+inference = Inference()
 
 app = FastAPI()
 
@@ -9,10 +17,7 @@ app = FastAPI()
 def predict(request: InferenceRequest) -> InferenceResponse:
     text = request.text
 
-    # tymczasowa logika "predykcji"
-    if "great" in text.lower():
-        prediction = "positive"
-    else:
-        prediction = "neutral"
+    # Prawdziwa predykcja z modelu
+    prediction = inference.predict(text)
 
     return InferenceResponse(prediction=prediction)
